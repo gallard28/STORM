1da4ed7442f3390de76c91780969775cc7ef9400
gallard28 <gallard@clemson.edu>
2018-10-30 22:45:09 -0400

    bedtime on tuesday

--- Storm Code.R
+++ Storm Code.R
@@ -72,7 +72,7 @@ data<-xmlSApply(rootNode,function(x) xmlSApply(x, xmlValue))
 data_df<-data.frame(t(data), row.names=NULL)
 
 
-#Loop for 2017
+#Loop for 2017####
 Folder<-'/Users/GrantAllard/Documents/Allard Scholarship/Conferences and Journals - CFPs, Etc. /ASIS&T 2018/Cyberinfrastructure poster/Data and Analysis/NSF Data/2017/'
 Files<-read.delim('/Users/GrantAllard/Documents/Allard Scholarship/Conferences and Journals - CFPs, Etc. /ASIS&T 2018/Cyberinfrastructure poster/Data and Analysis/NSF Data/2017/FileNames.txt')
 Files<-as_data_frame(Files[-c(1:6),])
@@ -106,3 +106,46 @@ Data2017Backup<-Data2017
 ExportFile<-paste(Folder,sep="","Data2017",".csv")
 write.csv(Data2017, ExportFile)
 
+#Loop for 2016####
+Year<-'2016'
+Base<-'/Users/GrantAllard/Documents/Allard Scholarship/Conferences and Journals - CFPs, Etc. /ASIS&T 2018/Cyberinfrastructure poster/Data and Analysis/NSF Data/'
+Folder<-paste(Base,sep="",Year)
+Folder<-paste(Folder, sep="/", Year)
+Files<-data.frame(list.files(Folder))
+colnames(Files)[1]<-"files"
+Files$path<-paste(Folder, sep="/", Files$files)
+
+
+#Create new dataframe
+name<-paste("data",sep="",Year)
+
+
+for (i in 1:length(Files$path)){
+  tryCatch({
+    start<-Sys.time()
+    print(paste("start loop", sep="", start))
+    xmldoc<-xmlParse(Files$path[i])
+    rootNode<-xmlRoot(xmldoc)
+    data<-xmlSApply(rootNode,function(x) xmlSApply(x, xmlValue))
+    data_df<-data.frame(t(data), row.names=NULL)
+    data_df$file<-Files$path[i]
+    Data2016<-smartbind(Data2016,data_df)
+    end<-Sys.time()
+    print(paste("end loop", Files$path[i], sep="", end))
+  }, error=function(e){})
+}
+
+Data2016Backup<-Data2016
+
+#Write file to Folder
+ExportFile<-paste(Folder,sep="","Data2016",".csv")
+write.csv(Data2016, ExportFile)
+
+
+#Exploratory Data Analysis
+str(Data2016)
+
+Data2016$AwardTitle<-as.character(Data2016$AwardTitle)
+
+CI<-Data2016 %>% 
+  filter(str_detect(AwardTitle, "cyberinfrastructure") | str_detect(AwardTitle, "Cyberinfrastructure") )
