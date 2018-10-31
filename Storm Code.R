e14db9320bb0c68f5f297b4e2aaea6c40d981279
gallard28 <gallard@clemson.edu>
2018-10-31 15:34:45 -0400

    Cleaned data

--- Storm Code.R
+++ Storm Code.R
@@ -19,8 +19,10 @@ library(rvest)
 library(jsonlite) 
 library(httr)
 library(tidyverse)
-library(read)
+library(readxl)
 library(gtools)
+library(lubridate)
+library(plotly)
 
 #Install Packages
 
@@ -36,6 +38,7 @@ APIdata<-read.csv(csvloc)
 #Format data
 APIdata$date<-as.Date(APIdata$date, format="%m/%d/%Y")
 APIdata$startDate<-as.Date(APIdata$startDate, format="%m/%d/%Y")
+APIdata$expDate<-as.Date(APIdata$expDate, format="%m/%d/%Y")
 str(APIdata)
 
 #Investigate data - Found 10 results for Michener
@@ -170,7 +173,6 @@ names(Data2016)
 
 
 #Data Exploration####
-
 str(APIdata)
 APIdata$awardee<-as.character(APIdata$awardee)
 APIdata$awardeeName<-as.character(APIdata$awardeeName)
@@ -205,4 +207,75 @@ ggplot(APIdata, aes(x=awardeeName))+
 
 ggplot(APIdata, aes(x=transType))+
   geom_bar()
+
+
+names(APIdata)
+str(APIdata)
+
+#Clean Data
+APIdata$agency<-as.character(APIdata$agency)
+APIdata$awardeeAddress<-as.character(APIdata$awardeeAddress)
+APIdata$awardeeCity<-as.character(APIdata$awardeeCity)
+APIdata$awardeeCounty<-as.character(APIdata$awardeeCounty)
+APIdata$awardeeDistrictCode<-as.character(APIdata$awardeeDistrictCode)
+APIdata$awardeeStateCode<-as.character(APIdata$awardeeStateCode)
+APIdata$awardeeZipCode<-as.numeric(APIdata$awardeeZipCode)
+APIdata$dunsNumber<-as.character(APIdata$dunsNumber)
+APIdata$duns<-APIdata$dunsNumber
+APIdata$dunsNumber<-NULL
+APIdata$id<-as.character(APIdata$id)
+APIdata$parentDunsNumber<-as.character(APIdata$parentDunsNumber)
+APIdata$pdPIName<-as.character(APIdata$pdPIName)
+APIdata$perfAddress<-as.character(APIdata$perfAddress)
+APIdata$perfCity<-as.character(APIdata$perfCity)
+APIdata$perfCountryCode<-as.character(APIdata$perfCountryCode)
+APIdata$perfCounty<-as.character(APIdata$perfCounty)
+APIdata$perfLocation<-as.character(APIdata$perfLocation)
+APIdata$perfStateCode<-as.character(APIdata$perfStateCode)
+APIdata$awardeeZipCode<-as.numeric(APIdata$awardeeZipCode)
+APIdata$piEmail<-as.character(APIdata$piEmail)
+APIdata$piFirstName<-as.character(APIdata$piFirstName)
+APIdata$piLastName<-as.character(APIdata$piLastName)
+APIdata$piMiddeInitial<-as.character(APIdata$piMiddeInitial)
+APIdata$piFullName<-paste(APIdata$piFirstName,APIdata$piMiddeInitial, APIdata$piLastName, sep=" ")
+APIdata$poEmail<-as.character(APIdata$poEmail)
+APIdata$poName<-as.character(APIdata$poName)
+APIdata$poName<-as.character(APIdata$poName)
+phone <- "([1-9][0-9]{2})[- .]([0-9]{3})[- .]([0-9]{4})"
+APIdata$piPhone<-str_replace(APIdata$piPhone,phone, phone)
+APIdata$poPhone<-str_replace(APIdata$piPhone,phone, phone  )
+APIdata$primaryProgram<-as.character(APIdata$primaryProgram)
+APIdata$title<-as.character(APIdata$title)
+str(APIdata)
+
+
+#Awards by Year####
+#Check data types
+str(APIdata$startDate)
+str(APIdata$expDate)
+
+APIdata$interval<- APIdata$startDate %--% APIdata$expDate
+str(APIdata$interval)
+
+#seconds to days calculation 
+secs_in_days<- (24*60*60)
+
+APIdata$duration<-as.numeric((as.duration(APIdata$interval))/secs_in_days)
+str(APIdata$duration)
+
+Duration_count<-APIdata %>% 
+  group_by(duration) %>% 
+  
+ggplot(Duration_count, aes(x=duration, y = n, color=n))+
+  geom_point()
+
+#Need to also recode into categorical variable for frequency analysis and to make it consumable to human beings. 
+
+lm
+
+#Awards by size
+
+
+
+
 
